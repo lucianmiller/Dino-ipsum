b484bebc224af575444c7c9a4fb870626b5dcced
Lucian Miller <lucian.miller185@gmail.com>
2021-12-15 09:14:07 -0800

    Add totalScore prototype to Game Object

--- src/game.js
+++ src/game.js
@@ -26,4 +26,9 @@ export default class Game {
       this.switchPlayers();
     }
   }
+
+  totalScore() {
+    this.players[this.currentPlayerIndex].score += this.currentScore;
+    this.currentScore = 0
+  }
 }
