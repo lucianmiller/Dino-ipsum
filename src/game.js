2cc345b4a51bdc63c181f7d691519d51edf9e010
Lucian Miller <lucian.miller185@gmail.com>
2021-12-17 11:10:25 -0800

    Delete unessassary prototypes

--- src/game.js
+++ src/game.js
@@ -1,34 +1,6 @@
 export default class Game {
-  constructor() {
-    this.players = [];
-    this.currentScore = 0;
-    this.currentPlayerIndex = 0;
-    this.maxPlayers = 2;
-  }
-  
-  addPlayer(...playerObjects) {
-    this.players.push(...playerObjects);
-  }
-
-  switchPlayers() {
-    if (this.currentPlayerIndex + 1 != this.maxPlayers) {
-      this.currentPlayerIndex += 1;
-    } else if (this.currentPlayerIndex + 1 === this.maxPlayers) {
-      this.currentPlayerIndex = 0;
-    }
-  }
-
-  calculateScore (points) {
-    if (points !== 1) {
-      this.currentScore += points
-    } else if (points === 1) {
-      this.currentScore = 0;
-      this.switchPlayers();
-    }
-  }
-
-  totalScore() {
-    this.players[this.currentPlayerIndex].score += this.currentScore;
-    this.currentScore = 0
+  constructor(playerName, gameWord) {
+    this.player = playerName;
+    this.gameWord = gameWord
   }
 }
