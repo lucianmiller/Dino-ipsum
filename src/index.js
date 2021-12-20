7212125ce903a5b684d36e159cde1f9126c17dee
Lucian Miller <lucian.miller185@gmail.com>
2021-12-20 10:55:03 -0800

    Create and array of letters and create buttons

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import './css/styles.css';
 import Game from './game.js';
 
 $(document).ready(function() {
+  // Making an array of lowercase Ascii codes for every letter in the alphabet
+  const alphaCharNumbers = Array.from(Array(26)).map((a, i) => i+65);
+  // Creating an alphabet array from Ascii codes for lowercase letters
+  const alphabet = alphaCharNumbers.map((charNum) => String.fromCharCode(charNum));
+  function createButtons(letterArray) {
+    letterArray.forEach((letter) => {
+      const button = `<button id=${letter} value=${letter}>${letter}</button>`;
+      $("#buttons").append(button);
+    });
+  }
+  createButtons(alphabet);
   $("form#game-info").submit(function(event) {
     event.preventDefault();
     const newGame = new Game(userName);
