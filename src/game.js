f1211a895ab711d2bb725f530524143b226fbebe
Lucian Miller <lucian.miller185@gmail.com>
2022-01-04 10:25:02 -0800

    Add player guess prototype

--- src/game.js
+++ src/game.js
@@ -1,18 +1,18 @@
 export default class Game {
   constructor(playerName, gameWord) {
     this.player = playerName;
-    this.gameWord = gameWord
+    this.gameWord = gameWord;
+    this.missedLetters = [];
+    this.blankWord = new Array(this.gameWord.length).fill('__');
   }
 
-  lettersInWord(letter) {
+  playerGuess(letter) {
     if (this.gameWord.includes(letter)) {
-      console.log(letter);
       console.log(this.gameWord.indexOf(letter));
+      return true;
     } else {
-      // later store missed letters somewhere
+      this.missedLetters.push(letter);
       return false;
     }
   }
-
-  playerGuess() 
 }
