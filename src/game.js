c8195ec3bb651d2299ed317b727e4c92b44205d1
Lucian Miller <lucian.miller185@gmail.com>
2022-01-07 11:46:22 -0800

    Replace blanks with correctly guessed letters

--- src/game.js
+++ src/game.js
@@ -4,18 +4,27 @@ export default class Game {
     this.gameWord = gameWord;
     this.gameWordArr = gameWord.split('');
     this.missedLetters = [];
+    this.blankWord = [];
+  }
+
+  createBlankArray() {
     this.blankWord = new Array(this.gameWord.length).fill('__');
   }
 
   playerGuess(guessLetter) {
     if (this.gameWord.includes(guessLetter)) {
+      let blankArr = this.blankWord;
       this.gameWordArr.forEach(function(char, index){
-        this.blankWord.splice(index, 1, guessLetter);
-      })
+        if (char === guessLetter) {
+          blankArr.splice(index, 1, guessLetter);
+        }
+      });
       return true;
     } else {
       this.missedLetters.push(guessLetter);
       return false;
     }
   }
+
+  // create prototype to check if player has won or lost
 }
