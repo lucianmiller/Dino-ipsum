31e788563f9e5a1a5e6fbf632fc1c8cdd41922f4
Lucian Miller <lucian.miller185@gmail.com>
2022-01-07 11:07:02 -0800

    Add Array to store game word

--- src/game.js
+++ src/game.js
@@ -2,16 +2,19 @@ export default class Game {
   constructor(playerName, gameWord) {
     this.player = playerName;
     this.gameWord = gameWord;
+    this.gameWordArr = gameWord.split('');
     this.missedLetters = [];
     this.blankWord = new Array(this.gameWord.length).fill('__');
   }
 
-  playerGuess(letter) {
-    if (this.gameWord.includes(letter)) {
-      console.log(this.gameWord.indexOf(letter));
+  playerGuess(guessLetter) {
+    if (this.gameWord.includes(guessLetter)) {
+      this.gameWordArr.forEach(function(char, index){
+        this.blankWord.splice(index, 1, guessLetter);
+      })
       return true;
     } else {
-      this.missedLetters.push(letter);
+      this.missedLetters.push(guessLetter);
       return false;
     }
   }
