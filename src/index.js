fa08d9ac3810e72a0a824f129f5ec8d6aadbc3fe
Lucian Miller <lucian.miller185@gmail.com>
2022-01-19 11:02:38 -0800

    Add promise and move code into .then statement

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Game from './game.js';
+import DinoIpsum from './dino-ipsum';
 
 function createButtons(letterArray) {
   letterArray.forEach((letter) => {
@@ -39,12 +40,16 @@ $(document).ready(function() {
   const alphaCharNumbers = Array.from(Array(26)).map((a, i) => i+65);
   // Creating an alphabet array from Ascii codes for lowercase letters
   const alphabet = alphaCharNumbers.map((charNum) => String.fromCharCode(charNum));
-  const newGame = new Game('Player1', 'apple');
-  console.log(newGame.blankWord.join(' '));
-  newGame.createBlankArray();
-  attachListeners(newGame);
-  createButtons(alphabet);
-  displayGame(newGame);
+  let promise = DinoIpsum.getDino();
+  promise.then(function(response) {
+    const newGame = new Game('Player1', 'apple');
+    newGame.createBlankArray();
+    attachListeners(newGame);
+    createButtons(alphabet);
+    displayGame(newGame);
+  }, function(error) {
+    $('#game-status').text(`There was an error processing your request: ${error}`);
+  });
   $("form#game-info").submit(function(event) {
     event.preventDefault();
     const userName = $("#name").val;
